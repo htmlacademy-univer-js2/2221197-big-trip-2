5f1445b1c52327095ebdd63102d22dd624ae47e5
Irina946 <Irina.Larina@urfu.me>
2023-04-04 14:31:12 +0500

    шаблонизация

--- public/src/main.js
+++ public/src/main.js
@@ -2,11 +2,19 @@ import { render, RenderPosition } from './render';
 import Filters from './view/filters';
 import MenuView from './view/menu';
 import Trip from './presenter/trip';
+import PointsModel from './model/trip-points-model';
+import { getPoints, getDestinations, getOffers } from './mock/point';
 
 const menuContainer = document.querySelector('.trip-controls__navigation');
 const filterContainer = document.querySelector('.trip-controls__filters');
 const tripContainer = document.querySelector('.trip-events');
-const tripPresent = new Trip({container: tripContainer});
+const tripPresent = new Trip(tripContainer);
+const points = getPoints();
+const offersByType = getOffers();
+const destinations = getDestinations();
+const pointsModel = PointsModel();
+
 render(new Filters, filterContainer, RenderPosition.BEFOREEND);
 render(new MenuView, menuContainer, RenderPosition.BEFOREEND);
-tripPresent.init();
+pointsModel.init(points, destinations, offersByType);
+tripPresent.init(pointsModel);
